17ab64bd9e3427dfb48c4cdb29c43734b7c5efcc
Aaryamann171 <aaryamann171@gmail.com>
2023-09-20 05:34:04 +0530

    added comments

--- download.md
+++ download.md
@@ -1,3 +1,4 @@
+### Wget command to download all `jpg` images from a webpage
 Replace `<url>` with the index page of your resource
 ```bash
 wget -nd -r -l 1 -A jpg <url>
--- script.py
+++ script.py
@@ -3,7 +3,13 @@ import sqlite3
 
 
 def add_images_to_db(path):
-    dir_list = os.listdir(path)
+    dir_list = os.listdir(path)  # lists all files from a directory
+
+    # remove files that are not jpg from the directory list
+    for file_name in dir_list:
+        if not file_name.endswith(".jpg"):
+            dir_list.remove(file_name)
+
     conn = sqlite3.connect("my_images.db")
     conn.execute('''CREATE TABLE IF NOT EXISTS images (
                     id INTEGER PRIMARY KEY,
@@ -11,6 +17,7 @@ def add_images_to_db(path):
                     image_data BLOB,
                     score REAL
                 )''')
+
     total_images = len(dir_list)
     curr_count = 1
     for image in dir_list:
@@ -19,16 +26,22 @@ def add_images_to_db(path):
             print(f"reading image {image}, {curr_count} of {total_images}...")
             image_data = file.read()
             image_name_split = image.split(".")
+
+            # clean up file name - change the below line according to your data
             image_name = "_".join(image_name_split[0:len(image_name_split)-1])
+
+            # write to db
             conn.execute("INSERT INTO images (image_name, image_data, score) VALUES (?, ?, ?)",
-                         (image_name, sqlite3.Binary(image_data), score))
+                         (image_name, sqlite3.Binary(image_data), score)
+                         )
             conn.commit()
         curr_count += 1
+
     conn.close()
 
 
 def main():
-    path = "/Users/aaryamann/code/hot_or_not/misc/images"
+    path = "/Users/aaryamann/code/hot_or_not/misc/images" # replace with the path of your data
     add_images_to_db(path)
 
 
